c14c5ce41819ff6f944112d0f23d367070b4aabf
Brandon Stone <42560473+KidLiberty@users.noreply.github.com>
2025-01-16 18:49:12 -0500

    Updated favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import '../styles/globals.css'
 
-export default MyApp
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const emojiFavicon = `
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36">
+    <text x="0" y="32" font-size="42">🧑🏻‍💻</text>
+  </svg>
+`;
+
+  const emojiFaviconUrl = `data:image/svg+xml,${encodeURIComponent(emojiFavicon)}`;
+
+  return (
+    <>
+      <Head>
+        <link rel='icon' href={emojiFaviconUrl} type="image/svg+xml" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
